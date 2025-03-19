311a0ae277ca25ce93eb4197ae03de6fc9fc1817
cassindy <cassindy.tw@gmail.com>
2025-03-19 22:27:02 +0800

    first commit

---
+++ scripts/test.js
@@ -0,0 +1,9 @@
+// let _cookie = document.cookie;
+// _cookie.lightNightMode = "light";
+
+
+// console.log("cookie: "+_cookie)
+
+document.cookie = "lightNightMode = light; path=/";
+
+console.log("document.cookie: "+document.cookie)
